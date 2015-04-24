182549501258c3ebecd1140db571081912e501bb
amaradjackson <Ajackson@atl.yearup.org>
2015-04-24 16:25:13 -0400

    jhdhioj

---
+++ Project1/src/Intro1.java
@@ -0,0 +1,14 @@
+
+public class Intro1 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello! I'm Amara. Welcome to the Java class");
+// Errors listed below
+		/* Delete a bracket
+		 * mispelle Public
+		 * take out the period after out
+		 */
+	}
+
+}
