043663221743a0d0d593e675d254181b233c8a60
amaradjackson <Ajackson@atl.yearup.org>
2015-04-24 16:04:56 -0400

    Intro.java

---
+++ Project1/src/Intro.java
@@ -0,0 +1,17 @@
+
+public class Intro 
+{
+
+	public static void main(String[] args) 
+	{
+	   System.out.println("Hello Amara! Welcome to the Java Class.");
+	   //Change the output to say "Hello <your name>. Welcome to Java Class"
+	   /*Remove the brackets, what do you see?
+	    * Can you produce any more errors? Clean them and upload to git
+	    */
+	   
+
+	}
+
+}
+
