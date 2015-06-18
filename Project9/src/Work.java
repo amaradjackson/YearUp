ff5f7e3ff892db61a54695d1e9e687745b374780
amaradjackson <Ajackson@atl.yearup.org>
2015-06-18 19:15:21 -0400

    Parent Class

---
+++ Project9/src/Work.java
@@ -0,0 +1,14 @@
+
+public class Work 
+{
+	protected String m_work = null;
+	protected String m_location = null;
+	protected String m_name = null;
+
+	protected void DoWork()
+	{
+		System.out.println(m_name);
+		System.out.println(m_work);
+		System.out.println(m_location);
+	}
+}
